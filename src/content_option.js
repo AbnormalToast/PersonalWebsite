2f8e130796cdfd2e4a5c8fee1c368b72ef362570
AbnormalToast <thegamer11164@gmail.com>
2023-05-15 11:55:19 +0100

    More Shit

--- src/content_option.js
+++ src/content_option.js
@@ -7,9 +7,61 @@ const meta = {
 const introdate = {
     title: "Jordan Godden - Lighting Designer",
     animated: {
-        first: "I love Lighting Design"
-        second: "Well versed in GrandMA2"
+        first: "I love Lighting Design",
+        second: "Well versed in GrandMA2",
     },
     description: "[Add Description Here]",
     
 }
+
+const dataabout = {
+    title: "[Short intro about Myself]",
+    aboutme: "[Longer about me]"
+};
+const worktimeline = [{
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+    {
+        jobtitle: "",
+        where: "",
+        date: "",
+    },
+];
+
+const skills = [{
+        Name: "GrandMA2"
+    },
+    {
+        Name: "Chamsys MagicQ"
+    },
+];
+
+const services = [{
+
+}]
+
+const contactConfig = {
+    EMAIL: "",
+    description: "",
+
+};
+
+const socialprofiles = {
+    twitter: "twitter.com",
+    linkedin: "linkedin.com",
+    instagram: "instagram.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofiles,
+    logotext,
+};
